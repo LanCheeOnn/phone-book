48aa6e3669c5d592fb405c9ebb0853b6543faab0
Onn <cheeonn8@protonmail.com>
2021-09-21 02:05:21 +0800

    Implemented error checking if data could not be fetched

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ import LoadingIcons from 'react-loading-icons'
 const Home = () => {
   const [contacts, setContacts] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const handleDelete = (id) => {
     const newContacts = contacts.filter(contact => contact.id !== id);
@@ -12,18 +13,30 @@ const Home = () => {
   }
 
   useEffect(() => {
+    // Retrieves Data from json
     fetch('http://localhost:8000/contacts')
       .then(res => {
-        return res.json()
+        if (!res.ok) {
+          throw Error('Could not fetch the data');
+        }
+        return res.json();
       })
       .then(data => {
         setContacts(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch(err => {
+        setError(err.message);
         setIsLoading(false);
       })
   }, [])
 
   return (
     <div className="home">
+      {/* If error, it will be outputted. */}
+      {error && <div>{error}</div>}
+      {/* If Fetching data takes time, will show a loading icon and message */}
       {isLoading && <div className="Loading"><LoadingIcons.TailSpin
         height="4em"
         fill="#06bcee"
