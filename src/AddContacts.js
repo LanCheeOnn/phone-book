a6e587844d29910d519ce2c241f1ca192501f503
Onn <cheeonn8@protonmail.com>
2021-09-21 03:18:55 +0800

    Implemented form for adding contacts

--- src/AddContacts.js
+++ src/AddContacts.js
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 const AddContacts = () => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
   return (
     <div className="AddContacts">
       <h2>Add a new Contact</h2>
+      <form>
+        <label>Contact Name: </label>
+        <input
+          type="text"
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label>Contact Number: </label>
+        <input
+          type="text"
+          required
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+        <button>Add Contact</button>
+      </form>
     </div>
   );
 }
